fbe203e1222fb152d733d9ad2d126baddebda7bc
sumitnath2024 <162547457+sumitnath2024@users.noreply.github.com>
2024-06-19 16:00:49 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCw3D2XwcScZwIuRdByg_Bjs5MNWKdEq6A",
+  authDomain: "studymate-7fee1.firebaseapp.com",
+  databaseURL: "https://studymate-7fee1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "studymate-7fee1",
+  storageBucket: "studymate-7fee1.appspot.com",
+  messagingSenderId: "403602263334",
+  appId: "1:403602263334:web:63bd32bcb2562446ffc5e7",
+  measurementId: "G-JZLVWRCP5F"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
